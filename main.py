4f9626420a2037d63042ddc466086b080c07371b
Ahmed Quadri <ahmedquadri28@gmail.com>
2020-03-14 16:27:30 -0230

    Added Images

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('Libra_Unlabelled.png')
-# cv.imshow('Unlabelled', img)
+img = cv.imread('Constellations_Undetected/Gemini_Undetected.png')
+img_templ = cv.imread('Constellations_Template/Gemini.png')
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', img_gray)
+img_templ_gray = cv.cvtColor(img_templ, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', img_gray)
 threshold = 128
 ret, img_binary = cv.threshold(img_gray,127,255,cv.THRESH_BINARY)
+ret_temp, img_templ_binary = cv.threshold(img_templ_gray,127,255,cv.THRESH_BINARY)
 cv.imshow('Binary', img_binary)
+cv.imshow('Binary', img_templ_binary)
 view_mode = 1
-cv.waitKey(0);
+cv.waitKey(0)
+
 
